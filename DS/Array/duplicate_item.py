da1fcfa18a339db5522b39254f5afaadbbc6483f
alifhs <alif.hs@outlook.com>
2022-06-29 23:29:03 +0600

    add algo

--- DS/Array/duplicate_item.py
+++ DS/Array/duplicate_item.py
@@ -6,10 +6,10 @@
 # a[2] = -3 , i = 1 , val 2  index , refval = -3
 # a[3] = -4 , i = 2, val 3 index, refval = -4
 # a[4] = -3  , i = 3, val 4 index, refval = -3
-# a[4]       , i = 4 , val 3 index, refval = -4
+# a[3] which is already negative 4       , i = 4 , val 3 index, refval = -4
 
 
 # a[abs(a[i])] = -a[abs(a[i])]
 
-idea here is to look for a key (which is actually an array item) that
-previously modified an item (making the item negative)
+# idea here is to look for a key (which is actually an array item) that
+# previously modified an item (making the item negative)

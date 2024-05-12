ffdefceea1aab2a75185f7f7dee92b58cb92539d
alifhs <alif.hs@outlook.com>
2024-05-12 19:21:22 +0600

    add dfs solution

---
+++ Algorithm/GraphAlgorithm/BFS/bfs.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    visited.add(start)
+
+    while queue:
+        vertex = queue.popleft()
+        print(vertex, end=" ")
+
+        for neighbor in graph[vertex]:
+            if neighbor not in visited:
+                queue.append(neighbor)
+                visited.add(neighbor)
+
+# Example graph represented as an adjacency list
+graph = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+# Starting vertex for BFS traversal
+start_vertex = 'A'
+
+print("BFS Traversal:")
+bfs(graph, start_vertex)
